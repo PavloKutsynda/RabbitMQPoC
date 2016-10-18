1fe25e98262308322a45620d13cc8864fdd7bdc6
KutsyndaPavlo <kutsyndapavlo87@gmail.com>
2016-10-18 22:35:31 +0300

    update.

--- RabbitMQPoC/RabbitMQConsumer/Controllers/HomeController.cs
+++ RabbitMQPoC/RabbitMQConsumer/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQPoc.Service;
+using RabbitMQPoC.Model;
 
 namespace RabbitMQConsumer.Controllers
 {
@@ -10,7 +16,27 @@ namespace RabbitMQConsumer.Controllers
     {
         public ActionResult Index()
         {
+            RabbitMqService rabbitMqService = new RabbitMqService();
+            IConnection connection = rabbitMqService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+
+            ReceiveMessages(model);
             return View();
+        }
+
+        private void ReceiveMessages(IModel model)
+        {
+            model.BasicQos(0, 1, false);
+            QueueingBasicConsumer consumer = new QueueingBasicConsumer(model);
+            model.BasicConsume(RabbitMqService.QueueName, false, consumer);
+
+            while (true)
+            {
+                BasicDeliverEventArgs deliveryArguments = consumer.Queue.Dequeue() as BasicDeliverEventArgs;
+                String jsonified = Encoding.UTF8.GetString(deliveryArguments.Body);
+
+                RabbitMqMessage message = JsonConvert.DeserializeObject<RabbitMqMessage>(jsonified);
+            }
         }
 
         public ActionResult About()
