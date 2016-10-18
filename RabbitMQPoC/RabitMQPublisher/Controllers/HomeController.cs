37e5e3536d938bf0fd01d2ea2a5c33d2afc70ccc
KutsyndaPavlo <kutsyndapavlo87@gmail.com>
2016-10-18 22:15:21 +0300

    +

--- RabbitMQPoC/RabitMQPublisher/Controllers/HomeController.cs
+++ RabbitMQPoC/RabitMQPublisher/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQPoc.Service;
+using RabbitMQPoC.Model;
 
 namespace RabitMQPublisher.Controllers
 {
@@ -10,6 +15,13 @@ namespace RabitMQPublisher.Controllers
     {
         public ActionResult Index()
         {
+            RabbitMqService rabbitMqService = new RabbitMqService();
+            IConnection connection = rabbitMqService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+            
+            SetupSerialisationMessageQueue(model);
+            PublishMessage(model);
+
             return View();
         }
 
@@ -26,5 +38,35 @@ namespace RabitMQPublisher.Controllers
 
             return View();
         }
+
+        #region Private methods
+
+        private void SetupInitialTopicQueue(IModel model)
+        {
+            model.QueueDeclare("queueFromVisualStudio", true, false, false, null);
+            model.ExchangeDeclare("exchangeFromVisualStudio", ExchangeType.Topic);
+            model.QueueBind("queueFromVisualStudio", "exchangeFromVisualStudio", "superstars");
+        }
+
+        private void SetupSerialisationMessageQueue(IModel model)
+        {
+            model.QueueDeclare(RabbitMqService.QueueName, true, false, false, null);
+        }
+
+        private void PublishMessage(IModel model)
+        {
+            var message = new RabbitMqMessage()
+            {
+                Text = "Message"
+            };
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            basicProperties.SetPersistent(true);
+            String jsonified = JsonConvert.SerializeObject(message);
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(jsonified);
+            model.BasicPublish("", RabbitMqService.QueueName, basicProperties, messageBuffer);
+        }
+
+        #endregion
+
     }
 }
