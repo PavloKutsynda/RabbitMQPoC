51cb089e702631cef2e001519147a08bd05265cb
KutsyndaPavlo <kutsyndapavlo87@gmail.com>
2016-10-18 11:32:54 +0300

    Kick off.

---
+++ RabbitMQPoC/RabbitMQConsumer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RabbitMQConsumer.MvcApplication" Language="C#" %>
---
+++ RabbitMQPoC/RabbitMQConsumer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RabbitMQConsumer.Startup))]
+namespace RabbitMQConsumer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
