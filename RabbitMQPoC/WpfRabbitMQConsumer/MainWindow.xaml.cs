96c3817e308c3f50d25f24f75dd493137d7144db
KutsyndaPavlo <kutsyndapavlo87@gmail.com>
2016-10-19 11:27:16 +0300

    +

---
+++ RabbitMQPoC/WpfRabbitMQConsumer/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQPoc.Service;
+using RabbitMQPoC.Model;
+
+namespace WpfRabbitMQConsumer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt.Interval = TimeSpan.FromSeconds(1);
+            dt.IsEnabled = true;
+            dt.Tick += Dt_Tick;
+        }
+
+        private void Dt_Tick(object sender, EventArgs e)
+        {
+            this.TextBlock.Text = "";
+            this.TextBlock.Text = GetMessage();
+        }
+
+        private string GetMessage()
+        {
+            RabbitMqService rabbitMqService = new RabbitMqService();
+            IConnection connection = rabbitMqService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+
+            model.BasicQos(0, 1, false);
+            QueueingBasicConsumer consumer = new QueueingBasicConsumer(model);
+            model.BasicConsume(RabbitMqService.QueueName, false, consumer);
+
+            BasicDeliverEventArgs deliveryArguments;
+            consumer.Queue.Dequeue(500, out deliveryArguments);
+            if (deliveryArguments != null)
+            {
+
+                String jsonified = Encoding.UTF8.GetString(deliveryArguments.Body);
+                return jsonified;
+                //RabbitMqMessage message = JsonConvert.DeserializeObject<RabbitMqMessage>(jsonified);
+            }
+
+            return "";
+        }
+
+    }
+}
---
+++ RabbitMQPoC/WpfRabbitMqPublisher/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQPoc.Service;
+using RabbitMQPoC.Model;
+
+namespace WpfRabbitMqPublisher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Publish_FirstMessage(object sender, RoutedEventArgs e)
+        {
+            RabbitMqService rabbitMqService = new RabbitMqService();
+            IConnection connection = rabbitMqService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+
+            SetupSerialisationMessageQueue(model);
+            PublishFirstMessage(model);
+        }
+
+        private void Publish_SecondMessage(object sender, RoutedEventArgs e)
+        {
+            RabbitMqService rabbitMqService = new RabbitMqService();
+            IConnection connection = rabbitMqService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+
+            SetupSerialisationMessageQueue(model);
+            PublishSecondMessage(model);
+        }
+
+
+        #region Private methods
+
+        private void SetupInitialTopicQueue(IModel model)
+        {
+            model.QueueDeclare("queueFromVisualStudio", true, false, false, null);
+            model.ExchangeDeclare("exchangeFromVisualStudio", ExchangeType.Topic);
+            model.QueueBind("queueFromVisualStudio", "exchangeFromVisualStudio", "superstars");
+        }
+
+        private void SetupSerialisationMessageQueue(IModel model)
+        {
+            model.QueueDeclare(RabbitMqService.QueueName, true, false, false, null);
+        }
+
+        private void PublishFirstMessage(IModel model)
+        {
+            var message = new RabbitMqMessage()
+            {
+                Text = "First Message"
+            };
+
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            basicProperties.SetPersistent(true);
+            String jsonified = JsonConvert.SerializeObject(message);
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(jsonified);
+            model.BasicPublish("", RabbitMqService.QueueName, basicProperties, messageBuffer);
+        }
+
+        private void PublishSecondMessage(IModel model)
+        {
+            var message = new RabbitMqMessage()
+            {
+                Text = "Second Message"
+            };
+
+            IBasicProperties basicProperties = model.CreateBasicProperties();
+            basicProperties.SetPersistent(true);
+            String jsonified = JsonConvert.SerializeObject(message);
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(jsonified);
+            model.BasicPublish("", RabbitMqService.QueueName, basicProperties, messageBuffer);
+        }
+        #endregion
+    }
+}
